6e99f6cd7e33af5943677d70fc5d50ff5ace6289
ZachPingo <33404566+ZachPingo@users.noreply.github.com>
2017-11-12 02:02:22 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -222,17 +222,17 @@ bot.on("message", function(message) {
             message.channel.sendEmbed(onjoin);
             break;
 
-                    // User sends !staffCommands and it outputs staff commands
+        // User sends !staffCommands and it outputs staff commands
         case "staffCommands":
-        var staffCommands = new Discord.RichEmbed()
-            .addField("__!mute @(Name)__", "Mutes the member mentioned (Mod+)", true)
-            .addField("__!unmute @(Name)__","Unmutes the member mentioned (Mod+)", true)
-            .addField("__!kick @(Name)__", "Kicks the member mentioned (Admin+)")
-            .addField("__!ban @(Name)__", "Bans the member mentioned (Admin+)")
-            .setColor(0xc30000)
-            .setFooter("That is all the commands staff can do!")
-            .setTitle("__**Staff commands:**__")
-        message.channel.sendEmbed(staffCommands).catch(console.error);
+            var staffCommands = new Discord.RichEmbed()
+                .addField("__!mute @(Name)__", "Mutes the member mentioned (Mod+)", true)
+                .addField("__!unmute @(Name)__","Unmutes the member mentioned (Mod+)", true)
+                .addField("__!kick @(Name)__", "Kicks the member mentioned (Admin+)")
+                .addField("__!ban @(Name)__", "Bans the member mentioned (Admin+)")
+                .setColor(0xc30000)
+                .setFooter("That is all the commands staff can do!")
+                .setTitle("__**Staff commands:**__")
+        message.channel.sendEmbed(staffCommands);
         break;
 
         // If the command is invalid it outputs this message
